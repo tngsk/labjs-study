2b0f754d8bc2ee470da65da2f454430dae89b4a5
nagasaki <>
2021-05-30 21:36:46 +0900

    スクリプトでAOIを追加しようとするがうまくいかない

--- src/study.ts
+++ src/study.ts
@@ -1,4 +1,5 @@
 declare var lab: any
+declare var options: any
 
 const fixationCross = () => {
     return new lab.html.Screen({
@@ -13,7 +14,47 @@ const ds = new lab.data.Store()
 
 const study = new lab.flow.Sequence({
     content: [
-      fixationCross(),
+    fixationCross(),
+    new lab.html.Screen({
+      responses: {
+        'mouseup(@1)': '1',
+        'mouseup(@2)': '2',
+      },
+      messageHandlers: {
+        'before:prepare': function () {
+
+          (<any>this).options.content.push({
+            type: 'aoi',
+            left: 0,
+            top: 0,
+            width: 100,
+            height: 100,
+            label: '1'
+          });
+
+          (<any>this).options.content.push(
+          {
+            type: 'ellipse',
+            left: 0,
+            top: 0, 
+            width:100, // circle, lineの場合，ここのみ指定 textでは変化しない
+            height:100,
+            fill:'rgb(255,0,0)',// nullまたは'red' 'rgb(X,X,X)'
+            stroke:'black', // nullまたは'red' 'rgb(X,X,X)'
+            strokeWidth:'10',
+            angle: 0
+            })
+          
+          // (<any>this).options.events['click'] = (e: any) => {
+          //   console.log(e.clientX);
+          //   console.log(e.clientY);
+          //   console.log(document.documentElement.clientWidth);
+          //   console.log(document.documentElement.clientHeight);
+          //   (<any>this).end();
+          // }
+        }
+      }
+      }),
       new lab.html.Screen({
         content: '<div style="color: red"> blue </div>',
         datastore: ds,
